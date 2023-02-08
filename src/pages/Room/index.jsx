50ba6980aae33070814a4247277c03926e392040
NidhiTornekar <nidhitornekar15@gmail.com>
2023-02-08 13:32:16 +0530

    first commit

---
+++ src/pages/Room/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {useParams} from 'react-router-dom';
+import {ZegoUIKitPrebuilt} from '@zegocloud/zego-uikit-prebuilt';
+import { ZegoSuperBoardManager } from "zego-superboard-web";
+const RoomPage=() =>{
+    const {roomId}=useParams();
+    const myMeeting=async (element) =>{
+        const appID= 194356177;
+        const serverSecret="b2cbe30d8587066ee3e0538bf53fcbba";
+        const kitToken=ZegoUIKitPrebuilt.generateKitTokenForTest(appID,serverSecret,roomId,Date.now().toString(),"Nidhi");
+        const zc = ZegoUIKitPrebuilt.create(kitToken);
+        zc.addPlugins({ZegoSuperBoardManager});
+        zc.joinRoom({
+            container: element,
+            sharedLinks:[
+                {
+                    name:'Copy Link',
+                    url:`http://localhost:3000/room/${roomId}`,
+                }
+            ],
+            scenario: {
+                mode: ZegoUIKitPrebuilt.OneONoneCall,
+            },
+            showScreenSharingButton:true,
+            whiteboardConfig: {            
+                showAddImageButton: true, 
+             },
+        });
+    };
+    return <div>
+    <div ref={myMeeting} style={{ width: "100vw", height: "100vh" }}></div>;
+    </div>;
+};
+export default RoomPage;
