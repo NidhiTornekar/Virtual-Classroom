2a7bf2bda281074afe3c6de4e8a829de5ad28bb3
Nidhi Tornekar <121748841+NidhiTornekar@users.noreply.github.com>
2024-05-11 11:09:22 +0530

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,7 +10,7 @@ const HomePage = () =>{
    
 return( <div className="home">
         <div className='head'><h1>KESHAV MEMORIAL MEDICAL COLLEGE</h1></div>
-        <h1>VIRTUAL CLASSROOMS FOR MEDICAL STUDENTS</h1>
+        <h1>VIRTUAL CLASSROOM FOR TRAINING MEDICAL STUDENTS</h1>
         <div className="no">
             <br/>
         </div>
